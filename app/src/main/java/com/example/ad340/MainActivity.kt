6487fc32039992bf7fd1cb490aa6b90f46c9c056
Asia <Joanna.gromadzka@seattlecentral.edu>
2020-04-23 13:59:59 -0700

    from AS gui

--- app/src/main/java/com/example/ad340/MainActivity.kt
+++ app/src/main/java/com/example/ad340/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.ad340
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // referencing elements to variables
+        val zipcodeEditText: EditText = findViewById(R.id.zipcodeEditText)
+        val enterButton: Button = findViewById(R.id.enterButton)
+        //adding click listener to the button
+        enterButton.setOnClickListener {
+            //write "this"and context will pop, same with text just write the value ""
+            //Toast.makeText(this,"Button Clicked!",Toast.LENGTH_SHORT).show()
+            val zipcode: String = zipcodeEditText.text.toString()
+            if (zipcode.length !=5){
+                Toast.makeText(this,R.string.zipcode_entry_error,Toast.LENGTH_SHORT).show()
+            } else{
+                Toast.makeText(this,zipcode,Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
