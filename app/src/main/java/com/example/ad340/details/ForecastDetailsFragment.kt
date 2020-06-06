e94d10eabde2ee1b31779fb105be80792902e9fc
Asia <Joanna.gromadzka@seattlecentral.edu>
2020-06-06 13:09:17 -0700

    replacing findViewById() with ViewBinding saving data using ViewModel scoping

--- app/src/main/java/com/example/ad340/details/ForecastDetailsFragment.kt
+++ app/src/main/java/com/example/ad340/details/ForecastDetailsFragment.kt
@@ -6,9 +6,12 @@ import android.view.*
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
 import coil.api.load
 import com.example.ad340.*
+import com.example.ad340.databinding.FragmentForecastDetailsBinding
 import kotlinx.android.synthetic.main.fragment_forecast_details.*
 import kotlinx.android.synthetic.main.fragment_forecast_details.view.*
 import kotlinx.android.synthetic.main.fragment_forecast_details.view.dateTextFD
@@ -17,40 +20,50 @@ import java.util.*
 import kotlinx.android.synthetic.main.fragment_forecast_details.view.imageViewFD as imageViewFD1
 
 
-private val DATE_FORMAT = SimpleDateFormat("MM-dd-yyyy")
 
 class ForecastDetailsFragment : Fragment() {
     private val args: ForecastDetailsFragmentArgs by navArgs()
+
+    private lateinit var viewModelFactory: ForecastDetailsViewModelFactory
+
+    //"by" means we will use a delegate, which here it is saving for us a state so it doesn't rebuild when screen rotation happens
+    //it hands us back a viewModel which is automatically saved and cashed for us
+    private val viewModel: ForecastDetailsViewModel by viewModels(
+        factoryProducer = { viewModelFactory}
+    )
+
+    private var _binding: FragmentForecastDetailsBinding? = null
+    // this property is only valid between onCreateView and onDestroyView
+    private val binding get() = _binding!!
+    //private val binding get()= _binding!!
+
     private lateinit var tempDisplaySettingManager: TempDisplaySettingManager
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        val layout = inflater.inflate(R.layout.fragment_forecast_details,container,false)
-
+        _binding = FragmentForecastDetailsBinding.inflate(inflater, container, false)
+        viewModelFactory = ForecastDetailsViewModelFactory(args)
         tempDisplaySettingManager = TempDisplaySettingManager(requireContext())
 
-        val tempText = layout.findViewById<TextView>(R.id.tempText)
-        val descriptionText = layout.findViewById<TextView>(R.id.descriptionText)
-        val dateText = layout.findViewById<TextView>(R.id.dateTextFD)
-        val forecastIcon = layout.findViewById<ImageView>(R.id.imageViewFD)
+        return binding.root
+    }
 
-//        dateText.text = args.date.toString()
-
-        // dateText.text = DATE_FORMAT.format(Date(dailyForecast.date * 1000))   from DailyForecastAdapter
-
-                //   val iconId = dailyForecast.weather[0].icon
-                //   forecastIcon.load("http://openweathermap.org/img/wn/${iconId}@2x.png")
- //       forecastIcon.image= args.icon
-        val iconId = args.icon
-        forecastIcon.load("http://openweathermap.org/img/wn/${iconId}@2x.png")
-        dateText.text = DATE_FORMAT.format(Date(args.date* 1000))
-        //val temp = intent.getFloatExtra("key_temp",0f)
-        tempText.text = formatTempForDisplay(args.temp, tempDisplaySettingManager.getTempDisplaySetting())
-        descriptionText.text = args.description
-        return layout
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val viewStateObserver = Observer<ForecastDetailsViewState>{viewState ->
+        //update the UI
+            binding.tempText.text = formatTempForDisplay(viewState.temp, tempDisplaySettingManager.getTempDisplaySetting())
+            binding.descriptionText.text = viewState.description
+            binding.dateTextFD.text = viewState.date
+            binding.imageViewFD.load(viewState.iconUrl)
+        }
+        viewModel.viewState.observe(viewLifecycleOwner,viewStateObserver)
+        //adding following bonding get rids of the single icon screen
+        //viewModel.processArgs(args)
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
